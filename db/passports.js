9c7665e19cee39d0dc0020932972d945594ec48f
ryujm1828 <ryujm1828@gmail.com>
2023-04-08 23:12:26 +0900

    라우팅 정리

--- db/passports.js
+++ db/passports.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const passport = require("passport");
 const GitHubStrategy = require("passport-github2").Strategy;
-const express = require("express");
 const db = require("./db");
 
 module.exports = () =>{
@@ -9,12 +8,14 @@ module.exports = () =>{
     //로그인 최초 성공시 실행 되는 
     // done(null, user.id)로 세션을 초기화 한다.
     passport.serializeUser(function (user, done) {
+        
         done(null, user.id);
     });
-
+    
     // 사용자가 페이지를 방문할 때마다 호출되는 함수
     // done(null, id)로 사용자의 정보를 각 request의 user 변수에 넣어준다.
     passport.deserializeUser(function (id, done) {
+        
         done(null, id);
     });
     
@@ -25,11 +26,13 @@ module.exports = () =>{
             callbackURL: process.env.GITHUB_CALLBACK_URL 
         },
         function(accessToken, refreshToken, profile, done) {
+            
             //if(profile.id가 sql에 없으면 데이터 저장)
             const params = [profile.id]
             db.query(`SELECT * from USERS where ID=? ;`,params,function(err,rows,fields){
                 if(rows.length === 0){
                     //유저회원가입
+                   
                     db.query(`INSERT INTO USERS
                     (ID, Githubid, Nickname)
                     VALUES(${profile.id}, '${profile.username}', '${profile.username}');`,
@@ -41,6 +44,7 @@ module.exports = () =>{
                     console.log(`${profile.id} is already exist`);
                 }
             })
+            
             return done(null,profile);
         }
     ));
---
+++ router/board.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../db/db");
+const requestIp = require("request-ip");    //get ip
+const board_list = ["jayu","ik","security","board"];    //게시판 리스트
+const sanitizer = require("../middle/sanitizer");
+const logger = require('../log/logger');
+
+//board list send
+router.get("/:board/list",function(req,res){
+    res.send(db.query(""));
+})
+
+//write
+router.post("/:board/write_process", function (req, res,next) {
+    const ip = requestIp.getClientIp(req);
+    const title = req.body.title;
+    const content = req.body.content;
+    console.log(`title : ${title} content : ${content} user : ${req.user}`);
+    //title,content
+    //board가 없을 때 혹은 로그인이 안되어 있을 때 혹은 권한이 없을 때
+    if(board_list.includes(req.params.board) == false || !req.isAuthenticated()){
+      logger.info(`${req.method} / ip : ${ip} id : ${req.user} try post but fail $`);
+      //res.status(404).send('not found');
+    } else {
+        const params = [title, content, req.user,'TABS'];
+        db.query(`INSERT INTO BOARD
+        (Title, Content, USERS_ID,TAB)
+        VALUES(?,?,?,?);`,params,
+        function(err,rows,fields){
+            if(err) console.log(err);
+        });
+        logger.info(`${req.method} / ip : ${ip} id : ${req.user} post $ complete`);
+      //글쓰기
+    }
+    res.redirect("/");
+});
+  
+//update
+router.post("/:board/update_process",function (req, res) {
+    const ip = requestIp.getClientIp(req);
+    //board가 없을 때 혹은 다른 유저 일 때
+    if(board_list.includes(req.params.board) == false || req.user != db.query("") || !req.isAuthenticated()){
+      logger.info(`${req.method} / ip : ${ip} id : ${req.user} try update but fail $`);
+      res.status(404).send('not found');
+    } else {
+      logger.info(`${req.method} / ip : ${ip} id : ${req.user} update $ complete`);
+      //수정
+    }
+});
+
+module.exports = router;
