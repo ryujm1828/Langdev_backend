5070a42dd547fc32c1022b5d61589662c155855d
ryujm1828 <ryujm1828@gmail.com>
2023-04-11 13:29:52 +0900

    xss공격 방지 기능 추가

--- router/board.js
+++ router/board.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const db = require("../db/db");
 const requestIp = require("request-ip");    //get ip
 const board_list = ["jayu","ik","security","board"];    //게시판 리스트
+const cleanxss = require("../middle/sanitizer");
+
 //const sanitizer = require("../middle/sanitizer");
 const logger = require('../log/logger');
 
@@ -14,8 +16,8 @@ router.get("/:board/list",function(req,res){
 //write
 router.post("/:board/write_process", function (req, res,next) {
     const ip = requestIp.getClientIp(req);
-    const title = req.body.title;
-    const content = req.body.content;
+    const title = cleanxss(req.body.title);
+    const content = cleanxss(req.body.content);
     console.log(`title : ${title} content : ${content} user : ${req.user}`);
     //title,content
     //board가 없을 때 혹은 로그인이 안되어 있을 때 혹은 권한이 없을 때
@@ -23,6 +25,7 @@ router.post("/:board/write_process", function (req, res,next) {
       logger.info(`${req.method} / ip : ${ip} id : ${req.user} try post but fail $`);
       //res.status(404).send('not found');
     } else {
+        //글 작성
         const params = [title, content, req.user,'TABS'];
         let insertid;
         db.query(`INSERT INTO BOARD
@@ -42,6 +45,9 @@ router.post("/:board/write_process", function (req, res,next) {
 //update
 router.post("/:board/update_process",function (req, res) {
     const ip = requestIp.getClientIp(req);
+    const title = cleanxss(req.body.title);
+    const content = cleanxss(req.body.content);
+    
     //board가 없을 때 혹은 다른 유저 일 때
     if(board_list.includes(req.params.board) == false || req.user != db.query("") || !req.isAuthenticated()){
       logger.info(`${req.method} / ip : ${ip} id : ${req.user} try update but fail $`);
