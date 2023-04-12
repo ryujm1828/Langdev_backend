dd7c5af37c9aeb4a9172b1ecbada54ccaf30e4d7
ryujm1828 <ryujm1828@gmail.com>
2023-04-12 23:11:33 +0900

    소문자로 변경

--- router/api.js
+++ router/api.js
@@ -81,9 +81,21 @@ router.get("/board/list/all",function(req,res){
     const tab = req.query.tab;
     const page = 1;//req.query.page;
     const boardnum = 20;    //불러올 게시글 개수
-    db.query(`SELECT Title, POSTID, authorid,tab,category,isBest,good,bad FROM BOARD ORDER BY POSTID DESC LIMIT ${(page-1)*20},${boardnum}`,params,function(err,rows){
+    db.query(`SELECT title, postid, authorid,tab,category,isBest,good,bad FROM BOARD ORDER BY POSTID DESC LIMIT ${(page-1)*20},${boardnum}`,params,function(err,rows){
         console.log(rows);
         res.send(rows);
+        //게시글 목록 전송
+    });
+})
+
+router.get("/board/list/",function(req,res){
+    const params = ["category"];
+    const tab = req.query.tab;
+    const page = 1;//req.query.page;
+    const boardnum = 20;    //불러올 게시글 개수
+    db.query(`SELECT Title, POSTID, authorid,tab,category,isBest,good,bad FROM BOARD ORDER BY POSTID DESC LIMIT ${(page-1)*20},${boardnum}`,params,function(err,rows){
+        console.log(rows);
+        res.send(rows); 
         //게시글 목록 전송
     });
 })
