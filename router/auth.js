fd43942b3be921894e7d223ca193743f974453e6
ryujm1828 <ryujm1828@gmail.com>
2023-05-02 20:47:19 +0900

    깃헙 로그인

--- router/auth.js
+++ router/auth.js
@@ -6,22 +6,19 @@ const passport = require("passport");
 const crypto = require("crypto");
 
 //github 로그인 처리
-router.get('/login', passport.authenticate('local',{failureRedirect: '/login' }),
+router.get('/github', passport.authenticate('github',{
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+  successFlash: true
+}));
 
+//github 로그인 데이터 받아오기
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
-  res.redirect('/');
+    // Successful authentication, redirect home.
+    res.redirect('/');
 });
-
-router.post('/pwchange', function(req,res){
-  const changepw = req.body.changepw;
-  if(!req.isAuthenticated()){
-    res.status(404).send('not found');
-  }
-  else{
-    db.query("UPDATE pw ",function(){
-
-    })
-  }
-})
 
 module.exports = router;
